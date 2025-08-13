23644b29d5ec3d3b30aa8250b9b5657bb176f77e
jimpaul98 <jimpaul181@outlook.com>
2025-08-13 18:01:00 -0500

    cambio de Base

--- src/database.js
+++ src/database.js
@@ -1,5 +1,6 @@
 const mongoose = require ('mongoose');
-mongoose.connect('mongodb://localhost/uefbcs-app',{
+mongoose.connect('mongodb+srv://jimpaul181:UEFFBCS1234@ueffbcs.89wqbbh.mongodb.net/ueffbcs',
+{
 
 })
 .then(db=> console.log('Base de datos conectada'))
